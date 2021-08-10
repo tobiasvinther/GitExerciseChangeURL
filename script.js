b2d2450814bb8ad9f9e2e97125db37ec6c8d2e39
clbokea <clbo@kea.dk>
2021-08-10 12:07:55 +0200

    New script file added.
    Script.js from missing semester added to project.

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-/*$().ready(function() {
+$().ready(function() {
 	// $("h1:first").css("display", "none");
-	$("h1:first").html("Teknologi @ Kea");
-	//$(".page-header a").hide()
-	$(".page-header").append("<div>Dat20A & Dat20B forår 2021</div>")
-	//$(".page-header").append("<div>Underviser: Claus Bove, clbo@kea.dk</div>")
-	$(".main-content").prepend("<div><i><small><a href='index.html'>Undervisningsplan</a> </small></i></div>")
+	$("h1:first").html("./missing semester");
+	$(".page-header a:first").hide()
+	//$(".page-header").append("<div>Elective at KEA CS Fall 2021</div>")
+	$(".page-header").append("<div><a href='/au2021/' style='color:white; font-weight:bold '><img src='img/home-5-16.png'></a> | <a href='/au2021/about.html' style='color:white; font-weight:bold '>about</a> | <a href='/au2021/exam.html' style='color:white; font-weight:bold '>exam</a></div>");
+	//$(".page-header").append("<div style='text-align:right; margin-bottom: -70px;margin-top: 30px; '><a href='' style='color:white; font-weight:bold '>./missing_semester</a> | <a href='' style='color:white; font-weight:bold '>lectures</a> | <a href='' style='color:white; font-weight:bold '>about</a></div>")
 
-	$(".site-footer").append("<div style='text-align:right'>&copy; clbo@kea.dk, spring 2021</div>")
+	$(".site-footer").append("<div style='text-align:center'><small>Licensed under <a href='https://creativecommons.org/licenses/by-nc-sa/4.0/'>CC BY-NC-SA.</a> <br><a href='changelog.md'>Licence change log</a> <br> Claus Bove (clbo@kea.dk) <br> <a href='https://missing.csail.mit.edu/'> MIT - The Missing Semester</a></small></div>")
 	$(".site-footer-owner").hide()
+        $(".site-footer-credits").hide()	
+	$(".main-content").attr('style', 'max-width:60rem; font-size:1.2rem');
+	$(".page-header").attr('style', 'padding: 3rem 6rem');
+
 });
-*/
+
+/*
 
 $().ready(function() {
 		$("h1:first").css("display", "none");
@@ -17,4 +22,4 @@ $().ready(function() {
 		$(".markdown-body").prepend("<div><i><small>Dat20B & Dat20C, Teknologi 1, 2. Semester Forår 2021, Underviser: Claus Bove, clbo@kea.dk</small></i></div>")
 		$(".markdown-body").append("<div style='text-align:right'><i><small>&copy; clbo@kea.dk, Forår 2021</small></i></div>")	
 });
-
+*/
