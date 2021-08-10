4bddf1675be96579caa4804c2dc4d79dbfb1a939
clbokea <clbo@kea.dk>
2021-08-10 12:48:11 +0200

    script edited

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $().ready(function() {
 	// $("h1:first").css("display", "none");
-	$("h1:first").html("./missing semester");
+	//$("h1:first").html("./missing semester");
 	$(".page-header a:first").hide()
 	//$(".page-header").append("<div>Elective at KEA CS Fall 2021</div>")
-	$(".page-header").append("<div><a href='/au2021/' style='color:white; font-weight:bold '><img src='img/home-5-16.png'></a> | <a href='/au2021/about.html' style='color:white; font-weight:bold '>about</a> | <a href='/au2021/exam.html' style='color:white; font-weight:bold '>exam</a></div>");
+	$(".page-header").append("<div><a href='/e21/' style='color:white; font-weight:bold '><img src='img/home-5-16.png'></a>;
 	//$(".page-header").append("<div style='text-align:right; margin-bottom: -70px;margin-top: 30px; '><a href='' style='color:white; font-weight:bold '>./missing_semester</a> | <a href='' style='color:white; font-weight:bold '>lectures</a> | <a href='' style='color:white; font-weight:bold '>about</a></div>")
 
-	$(".site-footer").append("<div style='text-align:center'><small>Licensed under <a href='https://creativecommons.org/licenses/by-nc-sa/4.0/'>CC BY-NC-SA.</a> <br><a href='changelog.md'>Licence change log</a> <br> Claus Bove (clbo@kea.dk) <br> <a href='https://missing.csail.mit.edu/'> MIT - The Missing Semester</a></small></div>")
+	//$(".site-footer").append("<div style='text-align:center'><small>Licensed under <a href='https://creativecommons.org/licenses/by-nc-sa/4.0/'>CC BY-NC-SA.</a> <br><a href='changelog.md'>Licence change log</a> <br> Claus Bove (clbo@kea.dk) <br> <a href='https://missing.csail.mit.edu/'> MIT - The Missing Semester</a></small></div>")
 	$(".site-footer-owner").hide()
         $(".site-footer-credits").hide()	
 	$(".main-content").attr('style', 'max-width:60rem; font-size:1.2rem');
