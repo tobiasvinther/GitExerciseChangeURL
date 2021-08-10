34b0bb546bba7847da9881bcd17c84329ec53ba3
clbokea <clbo@kea.dk>
2021-08-10 12:50:09 +0200

    script edited

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $().ready(function() {
 	//$("h1:first").html("./missing semester");
 	$(".page-header a:first").hide()
 	//$(".page-header").append("<div>Elective at KEA CS Fall 2021</div>")
-	$(".page-header").append("<div><a href='/e21/' style='color:white; font-weight:bold '><img src='img/home-5-16.png'></a>;
+	$(".page-header").append("<div><a href='/e21/' style='color:white; font-weight:bold '><img src='img/home-5-16.png'></a>");
 	//$(".page-header").append("<div style='text-align:right; margin-bottom: -70px;margin-top: 30px; '><a href='' style='color:white; font-weight:bold '>./missing_semester</a> | <a href='' style='color:white; font-weight:bold '>lectures</a> | <a href='' style='color:white; font-weight:bold '>about</a></div>")
 
 	//$(".site-footer").append("<div style='text-align:center'><small>Licensed under <a href='https://creativecommons.org/licenses/by-nc-sa/4.0/'>CC BY-NC-SA.</a> <br><a href='changelog.md'>Licence change log</a> <br> Claus Bove (clbo@kea.dk) <br> <a href='https://missing.csail.mit.edu/'> MIT - The Missing Semester</a></small></div>")
