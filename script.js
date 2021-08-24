0300d7e3dbcc8d12756b14efcabae4a32e2dc133
clbokea <clbo@kea.dk>
2021-08-24 15:59:50 +0200

    link on home button ..

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $().ready(function() {
 	//$("h1:first").html("Teknologi 1 - KEA efterår 2021 - Kompendie");
 	$(".page-header a:first").hide()
 	//$(".page-header").append("<div>Elective at KEA CS Fall 2021</div>")
-	$(".main-content").prepend("Teknologi 1 - KEA efterår 2021 - Kompendie<div style='text-align: right'><a href='../'><img src='https://techkea.github.io/e21/img/home-5-24.png'></a>");
+	$(".main-content").prepend("Teknologi 1 - KEA efterår 2021 - Kompendie<div style='text-align: right'><a href='/e21/'><img src='https://techkea.github.io/e21/img/home-5-24.png'></a>");
 	//$(".page-header").append("<div style='text-align:right; margin-bottom: -70px;margin-top: 30px; '><a href='' style='color:white; font-weight:bold '>./missing_semester</a> | <a href='' style='color:white; font-weight:bold '>lectures</a> | <a href='' style='color:white; font-weight:bold '>about</a></div>")
 
 	$(".site-footer").append("<div style='text-align:center'><small>Licensed under <a href='https://creativecommons.org/licenses/by-nc-sa/4.0/'>CC BY-NC-SA.</a> <br> Claus Bove (clbo@kea.dk) efterår 2021 <br> <div>")
